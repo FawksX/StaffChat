4dce58726a29a75f6de08d06a20f08e1c6d096f1
FawksX <30556732+FawksX@users.noreply.github.com>
2021-04-29 10:12:04 +0100

    build: base project and dependencies

---
+++ src/main/java/games/synx/staffchat/StaffChatPlugin.java
@@ -0,0 +1,28 @@
+package games.synx.staffchat;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Dependency;
+import com.velocitypowered.api.plugin.Plugin;
+import org.slf4j.Logger;
+
+@Plugin(
+        id = "staffchat",
+        name = "StaffChat",
+        version = "@version@",
+        authors = {"FawksX"},
+        dependencies = {
+                @Dependency(id = "trevor")
+        }
+)
+public class StaffChatPlugin {
+
+    @Inject
+    private Logger logger;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+
+    }
+}
